892e68096933d8a45086dd1d177ec913d039f766
Yoanna Yordanova <yoanna.yordanova2402@gmail.com>
2025-03-31 14:57:13 +0300

    About me links

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-// Вземете елемента за бутона и менюто
+
+// header - menu
 const menuBtn = document.getElementById('menu-btn');
 const menuList = document.getElementById('menu-list');
 
-// Добавете събитие за клик върху бутона за менюто
 menuBtn.addEventListener('click', () => {
-    // Променя класа на менюто за показване или скриване
     menuList.classList.toggle('show');
 });
+
+// main - about me - tab-links
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
