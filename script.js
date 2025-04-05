9af20594f114044401e4af392c3ab8c118f8d23d
Yoanna Yordanova <yoanna.yordanova2402@gmail.com>
2025-04-05 14:20:12 +0300

    Contact me

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+// contact me
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('contact-form');
+    const status = document.getElementById('form-status');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault(); // спира презареждането
+        const data = new FormData(form);
+        
+        const response = await fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+        });
+
+        if (response.ok) {
+        status.innerHTML = "✅ Successfully sent!";
+        form.reset();
+        } else {
+        status.innerHTML = "❌ Something went wrong. Please try again.";
+        }
+    });
+});
